dd23353837d0e7f839274a951d7f5f91c2866566
Jonathan Claudius <jclaudius@mozilla.com>
2019-07-11 11:14:17 -0400

    Change report format

--- dirby/scan_engine.py
+++ dirby/scan_engine.py
@@ -7,22 +7,22 @@ class ScanEngine:
     def __init__(self, target, wordlist):
         self.target = target
         self.wordlist = wordlist
-        self.report = {}
+        self.report = []
 
     def request(self, url_path):
         r = requests.get(url_path)
-        return {"code": r.status_code}
+        return {"url": url_path, "code": r.status_code}
 
     def scan(self):
         for path in self.wordlist:
             url_path = self.target.url() + path
             json = self.request(url_path)
-            self.report[url_path] = json
+            self.report.append(json)
             sys.stderr.write("[+] " + url_path + "\n")
 
     def print_report(self):
         print(json.dumps({
-            'url': self.target.url(),
+            'base_url': self.target.url(),
             'host': self.target.host,
             'port': self.target.port,
             'scheme': self.target.scheme,
