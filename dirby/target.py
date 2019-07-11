c19612004923e09bb5107bdd6b8069a45745c3d5
Jonathan Claudius <jclaudius@mozilla.com>
2019-07-11 13:14:31 -0400

    Get make tests working again

--- dirby/target.py
+++ dirby/target.py
@@ -1,21 +1,19 @@
 class Target:
     def __init__(self, args):
-        self.scheme = args['scheme'] or 'https'
+        self.scheme = args['scheme']
         self.host = args['host']
-        self.port = int(args['port'] or 443)
-        if self.valid() == False:
-            raise RuntimeError("Invalid arguments passed to Target:" + str(args))
+        self.port = int(args['port'])
 
     def url(self):
         return "{}://{}:{}/".format(self.scheme, self.host, self.port)
 
-    def valid(self):
-        return self.valid_scheme()
+    # def valid(self):
+    #     return self.valid_scheme()
 
-    def valid_scheme(self):
-        return self.scheme == "http" or self.scheme == "https"
+    # def valid_scheme(self):
+    #     return self.scheme == "http" or self.scheme == "https"
 
-    def valid_port():
-        return self.port > 0 and self.port <= 65535
+    # def valid_port():
+    #     return self.port > 0 and self.port <= 65535
 
     # TODO: Add HOST validation
--- tests/test_target.py
+++ tests/test_target.py
@@ -6,10 +6,14 @@ from dirby.target import Target
 class TestTarget():
     def test_creation(self):
         host = 'foo.example.com'
-        args = {'host': host}
+        args = {
+            'host': host,
+            'port': 443,
+            'scheme': 'https'
+        }
         target = Target(args)
-        assert isinstance(target)
+        assert isinstance(target, Target)
         assert target.scheme == 'https'
         assert target.host == host
         assert target.port == 443
-        assert target.url() == "https://foo.example.com:443"
+        assert target.url() == "https://foo.example.com:443/"
